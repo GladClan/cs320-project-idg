c5bb2aec2c3bfad3b9811424d79df04bb621c4b5
Isaac Glad <idg862@ensign.edu>
2024-09-21 13:35:02 -0600

    Connect the frontend to the backend
    Made an input div so the user can input first and last name

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [message, setMessage] = useState('');
+    const [firstName, setFirst] = useState('');
+    const [lastName, setLast] = useState('');
+    const [submitted, setSubmitted] = useState(false);
+
+    const fetchMessage = async (e) => {
+        e.preventDefault();
+        const response = await fetch('/hello/name', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ first: firstName, last: lastName })
+        });
+        const text = await response.text();
+        setMessage(text);
+        // first !== '' && last !== '' ? setMessage('Hello, ' + first + ' ' + last + '!')
+        //     : first !== '' ? setMessage('Hello, ' + first + '!')
+        //         : setMessage("I'd like to know your name first!");
+        setSubmitted(true);
+    };
+
+    return (
+        <form className={"name-form"} onSubmit={fetchMessage}>
+            <h2>Personalized Greeting</h2>
+                <div className={"input"}>
+                    <p className={"name"}>First name:</p>
+                    <input type={"text"} value={firstName} onChange={(e) => setFirst(e.target.value)} />
+                </div>
+                <div className={"input"}>
+                    <p className={"name"}>Last name:</p>
+                    <input type={"text"} value={lastName} onChange={(e) => setLast(e.target.value)} />
+                </div>
+            <button className={"btn"} type={"submit"}>
+                Submit
+            </button>
+            {submitted && (
+                <div>
+                    <p className={"message"}>{message}</p>
+                </div>
+            )}
+        </form>
+    );
 }
 
 export default App;
